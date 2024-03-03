e865334613ad383037976efa051eee49c01fb721
Samitha Randika Edirisinghe <114610125+Samitha-Edirisinghe@users.noreply.github.com>
2024-03-04 00:26:37 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; Copyright © 2022 All Rights Reserved.</br>Design By  <a href="https://www.facebook.com/randika.nilupul/" target="blank" style="letter-spacing: 5px;">Samitha Randika</a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
